4d8d1adcacf12e01993ea1db86e8bb393ba19a89
Mailton Garrido Canuto <mgcfisio@hotmail.com>
2023-10-04 06:03:04 -0300

    Adicionar arquivos de projeto.

---
+++ Estudo c# for/Program.cs
@@ -0,0 +1,18 @@
+﻿//Inicializacao basica do for, variavel, condicao, incremento.
+
+for (int i= 0; i < 11; i++)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine();
+
+// for aninhado criando uma tabela de multiplicacao.
+
+for (int i = 1; i < 11; i++)
+{
+    for (int j = 0; j < 11; j++)
+    {
+        Console.WriteLine($"{i} X {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
